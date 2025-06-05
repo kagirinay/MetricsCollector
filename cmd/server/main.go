4e9d088c016962c29e7ea5f330e36fb7e89de39b
kagirinay <kagirinay@yandex.ru>
2025-06-05 19:58:14 +0700

    iter1.0

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,18 @@
 package main
 
+// MetricType описывает допустимые строковые идентификаторы типов метрик.
+type MetricType string
+
+const (
+	GaugeType   MetricType = "gauge"   // Плавающая метрика со значением - float64
+	CounterType MetricType = "counter" // Счётчик со значением - int64
+)
+
+// Gauge и Counter определены отдельными алиасами
+// только для лучшей читаемости кода.
+type (
+	Gauge   float64
+	Counter int64
+)
+
 func main() {}
