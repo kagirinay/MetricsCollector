84315a90c22ada41115ad519e2191b2e64826277
kagirinay <kagirinay@yandex.ru>
2025-07-01 18:18:55 +0700

    iter2.4

--- internal/agent/interfaces.go
+++ internal/agent/interfaces.go
@@ -1,2 +1,13 @@
 package agent
 
+import (
+	"io"
+	"net/http"
+)
+
+// Sender описывает минимальный HTTP-клиент, который необходим
+// непосредственно логике агента. Благодаря этому в тесттах можно
+// подменять стандартный *http.Client "заглушкой".
+type Sender interface {
+	Post(url, contentType string, body io.Reader) (*http.Response, error)
+}
