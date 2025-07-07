99d356ed0db6fc55a7f75e43609a407e773e0bad
kagirinay <kagirinay@yandex.ru>
2025-07-07 19:04:59 +0700

    iter2.6

--- internal/agent/scope.go
+++ internal/agent/scope.go
@@ -1,2 +1,7 @@
 package agent
 
+import "strconv"
+
+func formatFloat(f float64) string { return strconv.FormatFloat(f, 'f', -1, 64) }
+
+func formatInt(i int64) string { return strconv.FormatInt(i, 10) }
