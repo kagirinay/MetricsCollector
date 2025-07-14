bba4b66518b7463923094d57be2b2145e2d105e4
kagirinay <kagirinay@yandex.ru>
2025-07-14 18:30:01 +0700

    iter2.9

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/kagirinay/MetricsCollector.git/internal/agent"
+)
+
+func main() {
+	a := agent.New(&http.Client{}, "http://localhost:8080")
+	done := make(chan struct{})
+	go a.Run(done)
+	// Корректное завершение Ctrl+C
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+	close(done)
+	log.Println("Агент остановлен")
+}
