c68cbca5080c2a2117c047c489fac0ab90b045c7
kagirinay <kagirinay@yandex.ru>
2025-07-26 16:32:57 +0700

    iter3.1

--- internal/store/store.go
+++ internal/store/store.go
@@ -13,6 +13,8 @@ type Storage interface {
 	UpdateCounter(name string, delta models.Counter)
 	GetGauge(name string) (models.Gauge, bool)
 	GetCounter(name string) (models.Counter, bool)
+	GetAllGauge() map[string]models.Gauge
+	GetAllCounter() map[string]models.Counter
 }
 
 type MemStorage struct {
@@ -44,6 +46,7 @@ func (m *MemStorage) GetGauge(name string) (models.Gauge, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	v, ok := m.gauges[name]
+
 	return v, ok
 }
 
@@ -51,5 +54,30 @@ func (m *MemStorage) GetCounter(name string) (models.Counter, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	v, ok := m.counters[name]
+
 	return v, ok
+}
+
+func (m *MemStorage) GetAllGauge() map[string]models.Gauge {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	// Необходимо создать копию для безопасности и во избежание гонок.
+	copy := make(map[string]models.Gauge, len(m.gauges))
+	for k, v := range m.gauges {
+		copy[k] = v
+	}
+
+	return copy
+}
+
+func (m *MemStorage) GetAllCounter() map[string]models.Counter {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	// Необходимо создать копию для безопасности и во избежание гонок.
+	copy := make(map[string]models.Counter, len(m.counters))
+	for k, v := range m.counters {
+		copy[k] = v
+	}
+
+	return copy
 }
