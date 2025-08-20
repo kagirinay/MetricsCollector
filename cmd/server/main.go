69513308db37fe347fd72a7a491215d56e02739d
kagirinay <kagirinay@yandex.ru>
2025-08-20 17:32:24 +0700

    iter4.1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -10,9 +10,17 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/kagirinay/MetricsCollector.git/internal/handlers"
 	"github.com/kagirinay/MetricsCollector.git/internal/store"
+	"github.com/spf13/pflag"
 )
 
 func main() {
+	// Парсинг флагов
+	addr := pflag.StringP("address", "a", "localhost:8080", "HTTP server endpoint address")
+	pflag.Parse()
+	// Проверка на неизвестные флаги
+	if pflag.NArg() > 0 {
+		log.Fatalf("Неизвестный флаг или аргумент: %v", pflag.Args())
+	}
 	st := store.NewMemStorage()
 	r := chi.NewRouter()
 	// Регистрация обработчиков
@@ -20,14 +28,14 @@ func main() {
 	r.Get("/value/{type}/{name}", handlers.GetMetric(st))
 	r.Get("/", handlers.Home(st))
 	server := &http.Server{
-		Addr:    ":8080",
+		Addr:    *addr,
 		Handler: r,
 	}
 	// Безопасное завершение работы
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	go func() {
-		log.Println("Сервер запустился по адресу http://localhost:8080")
+		log.Printf("Сервер запустился по адресу http://%s", *addr)
 		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("Ошибка сервера: %v", err)
 		}
